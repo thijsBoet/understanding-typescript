4d766d1dde2bc48064d07286138db5e854afce98
Matthijs Boet <m.boet@chello.nl>
2020-11-30 02:33:09 +0100

    union types

--- 2-typescript-basics/course-starting-project/app.ts
+++ 2-typescript-basics/course-starting-project/app.ts
@@ -3,11 +3,14 @@
 //   age: number
 //   hobbies: string[]
 // } = {
+enum Status { ADMIN, READ_ONLY, AUTHOR }
+
 const person = {
   name: 'Matthijs',
   age: 30,
   hobbies: ['Programming', 'Playing guitar', 'Watching movies', 'Cooking'],
-  role: [2, 'author']
+  role: [2, 'author'],
+  status: Status.ADMIN 
 }
 
 console.log(person)
